e1b388ccc6b4af2df197806882d6336428483a1e
Lomeli <josea.lomeligar.scch@gmail.com>
2023-11-09 19:03:55 -0600

    commit  inical

---
+++ pages/demo.js
@@ -0,0 +1,9 @@
+export default function Demo() {
+    return (
+        <>
+      <main className='h-screen w-screem bg-gray-500'>
+        <h1 className='text-4x1 text-center text-white'>Hola</h1>
+      </main>
+      </>
+    )
+}
